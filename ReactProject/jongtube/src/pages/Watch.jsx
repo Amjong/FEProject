edcb658df6d8344967342fe7cc141a6be1683ac2
Jongbum <amjong2@gmail.com>
2022-12-23 19:29:16 +0900

    jongtube: Add title, channelTitle, description at Watch page

--- ReactProject/jongtube/src/pages/Watch.jsx
+++ ReactProject/jongtube/src/pages/Watch.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import VideoList from '../component/VideoList';
 import VideoPlayer from '../component/VideoPlayer';
 
 export default function Watch() {
   const { videoID } = useParams();
+  const [snippet, setSnippet] = useState({});
+  const fetchingVideo = () => {
+    fetch(`../data/videosByVideoID_Snippet.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSnippet({
+          title: data.items[0].snippet.title,
+          channelTitle: data.items[0].snippet.channelTitle,
+          description: data.items[0].snippet.description,
+        });
+      });
+  };
+  useEffect(fetchingVideo, []);
   return (
     <div>
       Watch {videoID}!<VideoPlayer></VideoPlayer>
+      <div>{snippet.title}</div>
+      <div>{snippet.channelTitle}</div>
+      <div>{snippet.description}</div>
     </div>
   );
 }
