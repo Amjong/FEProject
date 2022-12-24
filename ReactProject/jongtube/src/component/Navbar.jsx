d2323257374e240f462c04738c48c87826fb32c0
Jongbum <amjong2@gmail.com>
2022-12-24 23:21:04 +0900

    jongtube: Add gray line at Navbar

--- ReactProject/jongtube/src/component/Navbar.jsx
+++ ReactProject/jongtube/src/component/Navbar.jsx
@@ -14,23 +14,26 @@ export default function Navbar() {
     setText(e.target.value);
   };
   return (
-    <div className='flex justify-center'>
-      <FaYoutube className='w-10 h-10 inline text-red-600'></FaYoutube>
-      <span className='font-bold pr-10 leading-9 pl-1 hover:pointer-events-auto'>
-        JongTube
-      </span>
-      <form className='inline pl-10' onSubmit={handleSubmit}>
-        <input
-          className='border-gray border-2 leading-8 pointer-events-auto'
-          type='text'
-          placeholder='search...'
-          value={text}
-          onChange={handleChange}
-        />
-        <button className='pl-1 pointer-events-auto'>
-          <FaSearch></FaSearch>
-        </button>
-      </form>
-    </div>
+    <>
+      <div className='flex justify-center'>
+        <FaYoutube className='w-10 h-10 inline text-red-600'></FaYoutube>
+        <span className='font-bold pr-10 leading-9 pl-1 hover:pointer-events-auto'>
+          JongTube
+        </span>
+        <form className='inline pl-10' onSubmit={handleSubmit}>
+          <input
+            className='border-gray border-2 leading-8 pointer-events-auto'
+            type='text'
+            placeholder='search...'
+            value={text}
+            onChange={handleChange}
+          />
+          <button className='pl-1 pointer-events-auto'>
+            <FaSearch></FaSearch>
+          </button>
+        </form>
+      </div>
+      <div className='w-full h-1 border-gray border-2'></div>
+    </>
   );
 }
