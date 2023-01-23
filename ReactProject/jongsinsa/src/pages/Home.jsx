9be38b83bafd0f8c23a118b7a155afbc1ca210ac
Jongbum <amjong2@gmail.com>
2023-01-23 16:30:54 +0900

    [jongsinsa] Render productItems at Home Page

--- ReactProject/jongsinsa/src/pages/Home.jsx
+++ ReactProject/jongsinsa/src/pages/Home.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useFirebaseApp } from '../context/FirebaseContext';
+import ProductItem from '../components/ProductItem';
+import ProductDetail from './ProductDetail';
 
 export default function Home() {
-  return <div>Home Page!</div>;
+  const [productList, setProductList] = useState([]);
+  const { firebaseApp } = useFirebaseApp();
+  const getProductList = () => {
+    firebaseApp?.readProductList(setProductList);
+  };
+  useEffect(getProductList, []);
+  return (
+    <div>
+      Home Page!
+      {productList.length && (
+        <ul>
+          {productList.map((id) => (
+            <ProductItem key={id} productId={id}></ProductItem>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
