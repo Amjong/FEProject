6e4ea7e406cc66a92be4b71fd98f8a4bbfa13542
Jongbum <amjong2@gmail.com>
2023-01-23 21:42:48 +0900

    [jongsinsa] Add onclick navigate to productDetail page

--- ReactProject/jongsinsa/src/components/ProductItem.jsx
+++ ReactProject/jongsinsa/src/components/ProductItem.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useFirebaseApp } from '../context/FirebaseContext';
 
 export default function ProductItem({ productId }) {
   const [product, setProduct] = useState({});
   const { firebaseApp } = useFirebaseApp();
+  const navigate = useNavigate();
   const getProductItem = () => {
     firebaseApp?.readProduct(productId, setProduct);
   };
   useEffect(getProductItem, []);
+  const handleClick = () => {
+    navigate(`product/${productId}`);
+  };
   return (
-    <li className='flex flex-col pt-10 m-3 border-2 rounded-md'>
+    <li
+      className='flex flex-col pt-10 m-3 border-2 rounded-md cursor-pointer'
+      onClick={() => {
+        handleClick();
+      }}
+    >
       <img src={product?.imageURL} alt='productImage' />
       <div className='whitespace-pre-line'>
         <span>{product?.description}</span>
