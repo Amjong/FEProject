d231825616aec0b9f56261b46e4c88630bd1ef5d
Jongbum <amjong2@gmail.com>
2023-01-23 23:39:02 +0900

    [jongsinsa] Add Home Link to Logo image

--- ReactProject/jongsinsa/src/components/Header.jsx
+++ ReactProject/jongsinsa/src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { FaShoppingCart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Login from './Login';
 
 export default function Header() {
   return (
-    <div className='flex'>
-      <img
-        className='w-1/5 '
-        src='https://res.cloudinary.com/dazzvmx3y/image/upload/v1673142546/Logo_yjedbj.png'
-        alt='Logo'
-      ></img>
+    <div className='flex mb-3 border-b border-zinc-300'>
+      <Link to='/'>
+        <img
+          className='w-1/5 '
+          src='https://res.cloudinary.com/dazzvmx3y/image/upload/v1673142546/Logo_yjedbj.png'
+          alt='Logo'
+        ></img>
+      </Link>
       <span className='ml-auto'>Products</span>
       <FaShoppingCart />
       <Login className='ml-auto'></Login>
