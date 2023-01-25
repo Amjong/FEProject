0fdfca5adbb321b19401d16c199e69d2ccf5ba75
Jongbum <amjong2@gmail.com>
2023-01-25 17:48:50 +0900

    [jongsinsa] Add text

--- ReactProject/jongsinsa/src/pages/ProductDetail.jsx
+++ ReactProject/jongsinsa/src/pages/ProductDetail.jsx
@@ -17,6 +17,7 @@ export default function ProductDetail() {
       <div className='w-2/5'>
         <h1>{product?.description}</h1>
         <div className='border-b border-zinc-300'>{product?.price} Gold</div>
+        [사이즈 선택]
         <Option options={product?.options} setOption={setOption}></Option>
         <button className='bg-zinc-600 px-4 text-white'>장바구니에 추가</button>
       </div>
