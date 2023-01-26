b6c26e00973c27f61c675c679361b1007efae2eb
Jongbum <amjong2@gmail.com>
2023-01-26 11:46:52 +0900

    [jongsinsa] Add category info at product/productId page

--- ReactProject/jongsinsa/src/pages/ProductDetail.jsx
+++ ReactProject/jongsinsa/src/pages/ProductDetail.jsx
@@ -10,26 +10,29 @@ export default function ProductDetail() {
   } = useLocation();
   const [option, setOption] = useState(product?.options[0]);
   return (
-    <div className='flex flex-row'>
-      <img
-        className='w-3/5 mr-2 h-screen'
-        src={product?.imageURL}
-        alt='productImage'
-      ></img>
-      <div className='w-2/5'>
-        <h1>{product?.description}</h1>
-        <div className='border-b border-zinc-300'>{product?.price} Gold</div>
-        [사이즈 선택]
-        <Option options={product?.options} setOption={setOption}></Option>
-        <button
-          onClick={() => {
-            handleIncrease({ ...product, options: option });
-          }}
-          className='bg-zinc-600 px-4 text-white'
-        >
-          장바구니에 추가
-        </button>
+    <>
+      <div>{`> ${product?.categories}`}</div>
+      <div className='flex flex-row'>
+        <img
+          className='w-3/5 mr-2 h-screen'
+          src={product?.imageURL}
+          alt='productImage'
+        ></img>
+        <div className='w-2/5'>
+          <h1>{product?.description}</h1>
+          <div className='border-b border-zinc-300'>{product?.price} Gold</div>
+          [사이즈 선택]
+          <Option options={product?.options} setOption={setOption}></Option>
+          <button
+            onClick={() => {
+              handleIncrease({ ...product, options: option });
+            }}
+            className='bg-zinc-600 px-4 text-white'
+          >
+            장바구니에 추가
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
