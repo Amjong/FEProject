086f9d211d22a9c39ba672ff33be26775fade159
Jongbum <amjong2@gmail.com>
2023-01-29 12:50:59 +0900

    [jongsinsa][style] base styling for body, header

--- ReactProject/jongsinsa/src/components/Header.jsx
+++ ReactProject/jongsinsa/src/components/Header.jsx
@@ -8,33 +8,34 @@ export default function Header() {
   const { cartItems } = useCart();
   const [isAdmin, setIsAdmin] = useState(false);
   return (
-    <div className='flex mb-3 border-b border-zinc-300'>
-      <Link to='/'>
+    <div className='flex justify-between mb-3 border-b border-gray-300 p-2'>
+      <Link to='/' className='flex items-center text-4xl'>
         <img
           className='w-1/5 '
           src='https://res.cloudinary.com/dazzvmx3y/image/upload/v1673142546/Logo_yjedbj.png'
           alt='Logo'
         ></img>
       </Link>
-      <Link to='/products'>
-        <span className='ml-auto'>Products</span>
-      </Link>
-      <Link to='/carts'>
-        <FaShoppingCart className='w-6 h-6' />
-        <span>{cartItems.length}</span>
-      </Link>
-      {isAdmin && (
-        <Link to='/register'>
-          <FaPencilAlt className='w-6 h-6'></FaPencilAlt>
+      <nav className='flex items-center gap-4 font-semibold'>
+        <Link to='/products'>
+          <span className='ml-auto'>Products</span>
         </Link>
-      )}
-
-      <Login
-        className='ml-auto'
-        setAdminState={(adminState) => {
-          setIsAdmin(adminState);
-        }}
-      ></Login>
+        <Link to='/carts'>
+          <FaShoppingCart className='w-6 h-6' />
+          <span>{cartItems.length}</span>
+        </Link>
+        {isAdmin && (
+          <Link to='/register'>
+            <FaPencilAlt className='w-6 h-6'></FaPencilAlt>
+          </Link>
+        )}
+        <Login
+          className='ml-auto'
+          setAdminState={(adminState) => {
+            setIsAdmin(adminState);
+          }}
+        ></Login>
+      </nav>
     </div>
   );
 }
