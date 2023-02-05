ecfbfceb6e5785e8094d996b1bf9d2112443cade
Jongbum <amjong2@gmail.com>
2023-02-05 15:51:47 +0900

    [jongsinsa][styling] update style of cart page

--- ReactProject/jongsinsa/src/components/CartItem.jsx
+++ ReactProject/jongsinsa/src/components/CartItem.jsx
@@ -1,40 +1,53 @@
 import React from 'react';
-import { GoTrashcan } from 'react-icons/go';
 import { useCart } from '../context/CartContext';
+import { AiOutlinePlusSquare, AiOutlineMinusSquare } from 'react-icons/ai';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
 
-export default function CartItem({ item }) {
+export default function CartItem({
+  item,
+  count,
+  item: { imageURL, description, options, price },
+}) {
   const { handleIncrease, handleDecrease, handleRemove } = useCart();
   return (
-    <div className='flex flex-row'>
-      <img src={item?.item?.imageURL} alt='productImage' />
-      <div className='flex flex-col text-left'>
-        <span>{item?.item?.description}</span>
-        <span>{item?.item?.options}</span>
-        <span>{item?.item?.price}</span>
+    <li className='flex justify-between my-2 items-center'>
+      <img
+        className='w-24 md:w-48 rounded-lg'
+        src={imageURL}
+        alt='productImage'
+      />
+      <div className='flex-1 flex justify-between ml-4'>
+        <div className='basis-3/5'>
+          <p className='text-lg'>{description}</p>
+          <p className='text-xl font-bold text-red-500'>{options}</p>
+          <p>{`${price} Gold`}</p>
+        </div>
+        <div className='text-2xl flex items-center'>
+          <AiOutlinePlusSquare
+            className='transition-all hover:text-red-500 hover:scale-10 mx-2 rounded-lg'
+            onClick={() => {
+              handleIncrease(item);
+            }}
+          >
+            +
+          </AiOutlinePlusSquare>
+          {count}
+          <AiOutlineMinusSquare
+            className='transition-all hover:text-red-500 hover:scale-105 mx-2'
+            onClick={() => {
+              handleDecrease(item);
+            }}
+          >
+            -
+          </AiOutlineMinusSquare>
+          <RiDeleteBin5Fill
+            className='transition-all hover:text-red-500 hover:scale-105 cursor-pointer mx-2'
+            onClick={() => {
+              handleRemove(item);
+            }}
+          ></RiDeleteBin5Fill>
+        </div>
       </div>
-      <div className='flex flex-row text-right'>
-        <button
-          onClick={() => {
-            handleIncrease(item?.item);
-          }}
-        >
-          +
-        </button>
-        {item?.count}
-        <button
-          onClick={() => {
-            handleDecrease(item?.item);
-          }}
-        >
-          -
-        </button>
-        <GoTrashcan
-          className='cursor-pointer'
-          onClick={() => {
-            handleRemove(item?.item);
-          }}
-        ></GoTrashcan>
-      </div>
-    </div>
+    </li>
   );
 }
--- ReactProject/jongsinsa/src/pages/Carts.jsx
+++ ReactProject/jongsinsa/src/pages/Carts.jsx
@@ -1,48 +1,39 @@
 import React from 'react';
 import CartItem from '../components/CartItem';
+import PriceCard from '../components/PriceCard';
 import { useCart } from '../context/CartContext';
+import { BsFillPlusCircleFill } from 'react-icons/bs';
+import { FaEquals } from 'react-icons/fa';
 
+const SHIPPING = 3000;
 export default function Carts() {
   const { cartItems } = useCart();
+  const totalPrice = cartItems
+    ? cartItems.reduce((sum, item) => sum + item.count * item.item.price, 0)
+    : 0;
   return (
-    <div>
-      <h1 className='border-b border-black-300 text-center'>내 장바구니</h1>
+    <div className='p-8 flex flex-col'>
+      <h1 className='text-2xl text-center font-bold pb-4 border-b border-gray-300 '>
+        내 장바구니
+      </h1>
       {cartItems && (
-        <ul className='flex flex-col'>
+        <ul className='border-b border-gray-300 mb-8 p-4 px-28'>
           {cartItems.map((item) => (
             <li key={item.id}>
-              <CartItem item={item}></CartItem>
+              <CartItem item={item.item} count={item.count}></CartItem>
             </li>
           ))}
         </ul>
       )}
-      <div className='flex flex-row m-auto'>
-        <div>
-          상품 총액
-          {cartItems
-            ? cartItems.reduce(
-                (sum, item) => sum + item.count * item.item.price,
-                0
-              )
-            : 0}
-          Gold
-        </div>
-        <span>+</span>
-        <div>배송비 3000 Gold</div>
-        <span>=</span>
-        <div>
-          총액
-          {cartItems
-            ? cartItems.reduce(
-                (sum, item) => sum + item.count * item.item.price,
-                0
-              ) + 3000
-            : 0}
-          Gold
-        </div>
+      <div className='flex justify-between items-center mb-6 px-2 md:px-8 lg:px-16'>
+        <PriceCard text='상품 총액' price={totalPrice} />
+        <BsFillPlusCircleFill className='shrink-0' />
+        <PriceCard text='배송액' price={SHIPPING} />
+        <FaEquals className='shrink-0' />
+        <PriceCard text='총 가격' price={totalPrice + SHIPPING} />
       </div>
       <button
-        className='text-center'
+        className='text-center bg-red-500 text-white'
         onClick={() => {
           alert('현재 구매하기 기능은 구현되어 있지 않습니다!');
         }}
