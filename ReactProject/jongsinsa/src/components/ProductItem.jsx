9fd9a8d200d274ee70caeb1427afed3cc05c2ae6
Jongbum <amjong2@gmail.com>
2023-02-05 10:58:46 +0900

    [jongsinsa][styling] Update productItem style

--- ReactProject/jongsinsa/src/components/ProductItem.jsx
+++ ReactProject/jongsinsa/src/components/ProductItem.jsx
@@ -15,18 +15,17 @@ export default function ProductItem({ productId }) {
   };
   return (
     <li
-      className='flex flex-col pt-10 m-3 border-2 rounded-md cursor-pointer'
+      className='flex flex-col pt-10 m-3 border-2 rounded-lg shadow-md overflow-hidden cursor-pointer'
       onClick={() => {
         handleClick();
       }}
     >
-      <img src={product?.imageURL} alt='productImage' />
-      <div className='whitespace-pre-line rounded-lg shadow-md overflow-hidden cursor-pointer'>
-        <span className='truncate'>{product?.description}</span>
-        <span>{product?.price}</span>
-        <span>{product?.categories}</span>
-        <span>{product?.options}</span>
+      <img className='w-full' src={product?.imageURL} alt='productImage' />
+      <div className='mt-2 px-2 text-lg flex justify-between items-center'>
+        <h3 className='truncate'>{product?.description}</h3>
+        <p>{`${product?.price} Gold`}</p>
       </div>
+      <p className='mb-2 px-2 text-gray-600'>{product?.categories}</p>
     </li>
   );
 }
--- ReactProject/jongsinsa/src/pages/Products.jsx
+++ ReactProject/jongsinsa/src/pages/Products.jsx
@@ -12,7 +12,7 @@ export default function Products() {
   return (
     <div>
       {productList && (
-        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 p-4'>
           {productList.map((id) => (
             <ProductItem key={id} productId={id}></ProductItem>
           ))}
